15a87b965a80a164d2ceffc0eb32b124039b19bb
purushottam113 <shreepad113@gmail.com>
2025-02-27 11:31:51 +0530

    Created a Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
+import NavBar from "./NavBar"
+
 function App() {
 
   return (
     <>
-      <h1>Hello world</h1>
+      <NavBar/>
+      <h1 className="text-3xl">Hello world</h1>
     </>
   )
 }
